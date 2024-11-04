16c4a8585934b7f4c7ba16906e9c8e42ba33b550
kulotskiI <ishielmishma24@gmail.com>
2024-11-04 22:12:54 +0800

    initial commit

---
+++ userProfile.js
@@ -0,0 +1,16 @@
+// Create a user profile object
+let userProfile = {
+    name: "Ishiel Mishma Ganaban",
+    age: 20,
+    favoriteNumber: 24,
+    favoriteColors: ["black", "white", "gray"]
+};
+
+// Print the user profile object
+console.log("User Profile:", userProfile);
+
+// Access and print specific properties
+console.log("Name:", userProfile.name);
+console.log("Age:", userProfile.age);
+console.log("Favorite Number:", userProfile.favoriteNumber);
+console.log("Favorite Colors:", userProfile.favoriteColors.join(", "));
